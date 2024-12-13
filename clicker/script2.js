f58dbade3db100f2cac64afa9ca2daf158d1cc24
pulkka <eero.j.laisi@gmail.com>
2024-12-13 18:18:06 +0200

    lololol

--- clicker/script2.js
+++ clicker/script2.js
@@ -6,31 +6,37 @@ var click = 1
 var asia1hin = 10
 var brsec = 0
 var asia2hin = 100
+var munch = new Audio('MUNCH.mp3')
 setInterval(function(){
     laskuri = laskuri + brsec
     borgors.innerHTML=""+laskuri
 },4000)
+setInterval(function(){
+    borgors.innerHTML=""+laskuri
+},100)
 function addborgor(){
     laskuri=laskuri+click
     borgors.innerHTML=""+laskuri
+    munch.play()
 }
 function buyshop(lol){
     if(lol==1){
         if (laskuri >= asia1hin){
             laskuri=laskuri-asia1hin
-            asia1hin = (Math.round(asia1hin * 3))
+            asia1hin = (Math.round(asia1hin * 3.1))
             borgors.innerHTML=""+laskuri
-            asia1.innerHTML="price:"+asia1hin
+            asia1.innerHTML=""+asia1hin
         }
         
     }
     if (lol == 2){
-        if (laskuri>=asia2hin){
+        if (laskuri >= asia2hin){
             brsec = brsec+1
-            asia2hin = (Math.round(asia2hin * 1.3))
-            asia2.innerHTML="price:"+asia2hin   
-            borgors.innerHTML=""+laskuri
             laskuri=laskuri-asia2hin
+            asia2hin = (Math.round(asia2hin * 1.3))
+            asia2.innerHTML=""+asia2hin   
+            borgors.innerHTML=""+laskuri
+            
             
         }
     }
